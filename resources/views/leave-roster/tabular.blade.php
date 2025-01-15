445a7382e4fa2a2c3a84866bd6f15e2e2567212c
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-01-15 16:12:33 +0300

    new update

--- resources/views/leave-roster/tabular.blade.php
+++ resources/views/leave-roster/tabular.blade.php
@@ -99,7 +99,7 @@
                             }
                         }, {
                             field: 'total_leave_roster_days',
-                            title: 'LEAVE DAYS ENTITLED',
+                            title: 'No. OF LEAVE DAYS',
                             sortable: true,
                             formatter: function(value) {
                                 return `<span class="badge bg-success">${value}</span>`;
