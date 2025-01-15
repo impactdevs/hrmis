932c21d8ee4d72744b82bf941ed59b8e930bcf78
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-01-15 15:33:34 +0300

    new update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,6 +15,14 @@
             </li>
 
             <li class="nav-item">
+                <a class="nav-link text-white d-flex align-items-center gap-2 fs-5 fw-bold @if (request()->routeIs('leave-roster.index') || request()->routeIs('leave-roster-tabular.index')) bg-secondary @endif"
+                    href="{{ route('leave-roster.index') }}">
+                    <i class="bi bi-calendar-plus"></i>
+                    {{ auth()->user()->isAdminOrSecretary() ? 'Leave Roster' : 'My Leave Roster' }}
+                </a>
+            </li>
+
+            <li class="nav-item">
                 <a class="nav-link text-white d-flex align-items-center gap-2 fs-5 fw-bold @if (request()->routeIs('leaves.index')) bg-secondary @endif"
                     href="{{ route('leaves.index') }}">
                     <i class="bi bi-bus-front"></i>
@@ -22,13 +30,6 @@
                 </a>
             </li>
 
-            <li class="nav-item">
-                <a class="nav-link text-white d-flex align-items-center gap-2 fs-5 fw-bold @if (request()->routeIs('leave-roster.index') || request()->routeIs('leave-roster-tabular.index')) bg-secondary @endif"
-                    href="{{ route('leave-roster.index') }}">
-                    <i class="bi bi-calendar-plus"></i>
-                    {{ auth()->user()->isAdminOrSecretary() ? 'Leave Roster' : 'My Leave Roster' }}
-                </a>
-            </li>
 
             @if (auth()->user()->isAdminOrSecretary())
                 <li class="nav-item">
