7d9ac78474091c8e9952ef1bbd4d79915867a540
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-03-21 17:12:05 +0300

    dashboard update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -28,9 +28,9 @@
                             </div>
                         </div><!-- End Sales Card -->
 
-                        <!-- Revenue Card -->
+                        <!-- Sales Card -->
                         <div class="col-xxl-3 col-md-6">
-                            <div class="card info-card revenue-card">
+                            <div class="card info-card sales-card">
 
                                 <div class="card-body">
                                     <h5 class="card-title">Attendees <span>| Today</span></h5>
@@ -47,7 +47,7 @@
                                 </div>
 
                             </div>
-                        </div><!-- End Revenue Card -->
+                        </div><!-- End Sales Card -->
 
                         <!-- Customers Card -->
                         <div class="col-xxl-3 col-xl-12">
@@ -64,29 +64,6 @@
                                         </div>
                                         <div class="ps-3">
                                             <h6>{{ $available_leave }}</h6>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-
-                        </div><!-- End Customers Card -->
-
-                        <!-- Customers Card -->
-                        <div class="col-xxl-3 col-xl-12">
-
-                            <div class="card info-card customers-card">
-
-                                <div class="card-body">
-                                    <h5 class="card-title">Birthdays <span>| Today</span></h5>
-
-                                    <div class="d-flex align-items-center">
-                                        <div
-                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                            <i class="bi bi-people"></i>
-                                        </div>
-                                        <div class="ps-3">
-                                            <h6>5</h6>
                                         </div>
                                     </div>
 
