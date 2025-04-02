7f5927c25ef8f9f3281917c22c53bbfc1423e508
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-04-02 10:14:25 +0300

    command update

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 //send birthday reminders
-Schedule::command('reminders:send-birthday')->weekly();
+Schedule::command('reminders:send-birthday')->daily();
