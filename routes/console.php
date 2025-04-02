38d06ad0e3bfdc143b816f8e0292a514d8969218
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-04-02 14:58:21 +0300

    timezone update

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 //send birthday reminders
-Schedule::command('reminders:send-birthday')->dailyAt('12:20');
+Schedule::command('reminders:send-birthday')->daily()->at('14:48');
