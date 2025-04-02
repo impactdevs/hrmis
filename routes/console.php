f02c6ffa6cdd0d7fc051f91078a444c8ef278e14
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-04-02 15:21:31 +0300

    schedular

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 //send birthday reminders
-Schedule::command('reminders:send-birthday')->daily()->at('15:20');
+Schedule::command('reminders:send-birthday')->daily();
