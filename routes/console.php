b23221a367ccc8d215e5209efa7d5d579ef1a10e
Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-06-17 12:46:29 +0300

    New update

--- routes/console.php
+++ routes/console.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('reminders:send-birthday')->daily();
 
 //send AppraisalReminder at every 1st of june
-Schedule::command('app:appraisal-reminder')->dailyAt('12:44');
+Schedule::command('app:appraisal-reminder')->yearlyOn(6, 1, '00:00');
 
