a362b38d310e2156da5a605ce3524af58cef564b
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-07-14 11:08:18 +0300

    attendances

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -3,23 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\Attendance;
+use App\Models\Employee;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AttendanceController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+
     public function index(Request $request)
     {
-        $date = $request->input('filter_date', now()->format('Y-m-d'));
+        $query = DB::table('attendances')
+            ->select(
+                'staff_id',
+                'access_date',
+                DB::raw('MIN(access_date_and_time) as clock_in'),
+                DB::raw('MAX(access_date_and_time) as clock_out')
+            )
+            ->groupBy('staff_id', 'access_date');
 
-        $attendances = Attendance::whereDate('attendance_date', $date)
-            ->with('employee') // Eager load employee
-            ->paginate(10);
+        if ($request->filled('filter_date')) {
+            $query->whereDate('access_date', $request->filter_date);
+        }
 
-        return view('attendances.index', compact('attendances'));
+        if ($request->filled('department_id')) {
+            $staffIds = Employee::where('department_id', $request->department_id)->pluck('staff_id');
+            $query->whereIn('staff_id', $staffIds);
+        }
+
+        $summarizedAttendances = $query->paginate(10);
+
+        return view('attendances.index', compact('summarizedAttendances'));
     }
+
+
 
 
     /**
--- resources/views/attendances/index.blade.php
+++ resources/views/attendances/index.blade.php
@@ -1,51 +1,66 @@
 <x-app-layout>
-    <div class="mt-3">
-        <div class="d-flex flex-row flex-1 justify-content-between mb-3">
-            <form action="{{ route('attendances.index') }}" method="GET" class="d-flex align-items-center">
-                <input type="date" name="filter_date" class="form-control"
+    <div class="mt-4">
+        <form method="GET" action="{{ route('attendances.index') }}" class="row g-3 mb-3">
+            <div class="col-md-4">
+                <label for="filter_date" class="form-label">Date</label>
+                <input type="date" name="filter_date" id="filter_date" class="form-control"
                     value="{{ request('filter_date', now()->format('Y-m-d')) }}">
-                <button type="submit" class="btn btn-primary ms-2">Filter</button>
-            </form>
-        </div>
+            </div>
+            @if (auth()->user()->hasRole('HR'))
+                <div class="col-md-4">
+                    <label for="department_id" class="form-label">Department</label>
+                    <select name="department_id" id="department_id" class="form-select">
+                        <option value="">All Departments</option>
+                        @foreach (\App\Models\Department::all() as $department)
+                            <option value="{{ $department->id }}"
+                                {{ request('department_id') == $department->id ? 'selected' : '' }}>
+                                {{ $department->department_name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            @endif
+            <div class="col-md-4 d-flex align-items-end">
+                <button type="submit" class="btn btn-primary w-100">Apply Filters</button>
+            </div>
+        </form>
 
-        <div class="table-wrapper">
-            <table class="table table-striped table-hover">
+        <div class="table-responsive">
+            <table id="attendance-table" class="table table-striped" data-toggle="table" data-pagination="true"
+                data-search="true" data-show-columns="true" data-show-export="true" data-click-to-select="true"
+                data-export-types="['csv', 'excel', 'pdf', 'print']" data-toolbar="#toolbar">
                 <thead class="table-light">
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Clock In Time</th>
+                        <th data-sortable="true">#</th>
+                        <th data-sortable="true">First Name</th>
+                        <th data-sortable="true">Last Name</th>
+                        <th data-sortable="true">Department</th>
+                        <th data-sortable="true">Date</th>
+                        <th data-sortable="true">Clock In</th>
+                        <th data-sortable="true">Clock Out</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @forelse ($attendances as $index => $attendance)
-                        <tr class="align-middle">
-                            <th scope="row">
-                                <a href="{{ route('attendances.show', $attendance->attendance_id) }}"
-                                    class="btn btn-outline-primary">
-                                    {{ $attendance->employee->staff_id }}
-                                </a>
-                            </th>
-                            <td>{{ $attendance->employee->first_name }}</td>
-                            <td>{{ $attendance->employee->last_name }}</td>
-                            <td>{{ \Carbon\Carbon::parse($attendance->attendance_date)->format('d-m-Y') }}</td>
+                    @forelse ($summarizedAttendances as $index => $attendance)
+                        @php
+                            $employee = \App\Models\Employee::where('staff_id', $attendance->staff_id)->first();
+                        @endphp
+                        <tr>
+                            <td>{{ $employee?->staff_id ?? 'N/A' }}</td>
+                            <td>{{ $employee?->first_name ?? 'N/A' }}</td>
+                            <td>{{ $employee?->last_name ?? 'N/A' }}</td>
+                            <td>{{ $employee?->department?->department_name ?? 'N/A' }}</td>
+                            <td>{{ \Carbon\Carbon::parse($attendance->access_date)->format('d-m-Y') }}</td>
                             <td>{{ \Carbon\Carbon::parse($attendance->clock_in)->format('H:i:s') }}</td>
+                            <td>{{ \Carbon\Carbon::parse($attendance->clock_out)->format('H:i:s') }}</td>
                         </tr>
                     @empty
                         <tr>
-                            <td colspan="5" class="text-center text-danger">
-                                No attendance records found for the selected date.
-                            </td>
+                            <td colspan="7" class="text-center text-danger">No attendance data available.</td>
                         </tr>
                     @endforelse
                 </tbody>
             </table>
-
-            <div class="pagination-wrapper">
-                {!! $attendances->appends(['filter_date' => request()->get('filter_date')])->render() !!}
-            </div>
         </div>
     </div>
 </x-app-layout>
